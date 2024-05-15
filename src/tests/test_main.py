d13b3744709c8b20c69bbf06ab53f8dcf7765c0b
Derek Wan <d.wan@icloud.com>
2024-05-15 16:05:56 +0900

    cleanup

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,16 +1,14 @@
 from __future__ import annotations
 
 from string import ascii_letters
-from typing import cast
+from typing import TYPE_CHECKING
 
 from hypothesis import assume, given
 from hypothesis.errors import InvalidArgument
 from hypothesis.extra.numpy import Shape, array_shapes, arrays
 from hypothesis.strategies import (
     DataObject,
-    DrawFn,
     SearchStrategy,
-    composite,
     data,
     integers,
     none,
@@ -18,31 +16,16 @@ from hypothesis.strategies import (
     text,
 )
 from numpy import ndarray, str_
-from numpy.typing import NDArray
+
+if TYPE_CHECKING:
+    from numpy.typing import NDArray
 
 
-def draw_text(
-    alphabet: str, /, *, min_size: int = 0, max_size: int | None = None
-) -> SearchStrategy[str]:
-    """Draw from a text-generating strategy."""
-    return text(alphabet, min_size=min_size, max_size=max_size)
-
-
-def text_ascii(*, min_size: int, max_size: int | None) -> SearchStrategy[str]:
-    """Strategy for generating ASCII text."""
-    return draw_text(ascii_letters, min_size=min_size, max_size=max_size)
-
-
-@composite
 def str_arrays(
-    draw: DrawFn, /, *, shape: Shape | None, min_size: int, max_size: int | None
-) -> NDArray[str]:
-    elements = text_ascii(min_size=min_size, max_size=max_size)
-    strategy = cast(
-        SearchStrategy[NDArray[str_]],
-        arrays(object, shape, elements=elements, fill=nothing(), unique=False),
-    )
-    return draw(strategy)
+    *, shape: Shape, min_size: int, max_size: int | None
+) -> SearchStrategy[NDArray[str_]]:
+    elements = text(ascii_letters, min_size=min_size, max_size=max_size)
+    return arrays(object, shape, elements=elements, fill=nothing(), unique=False)
 
 
 @given(
